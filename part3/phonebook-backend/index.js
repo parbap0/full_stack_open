d339e02f5525c8f6361e6f872e6ebdcdb979da42
parbap0 <parbap0@gmail.com>
2024-04-22 22:35:09 +0530

    solutions of 3.13-3.14: fetching data from database

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,6 +6,8 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
+require('dotenv').config()
+const Person = require('./models/person')
 
 morgan.token("data", (req, res) =>{
   return req.method === 'POST' ? JSON.stringify(req.body) : " "
@@ -15,28 +17,28 @@ app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms :data")
 );
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": 1,
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": 2,
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": 3,
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": 4,
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 
 
@@ -46,7 +48,9 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/info', (request, response) => {
@@ -56,14 +60,16 @@ app.get('/info', (request, response) => {
 
 app.get('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  
+  // const person = persons.find(person => person.id === id)
+  Person.findById(request.params.id).then(person => {
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
 
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+
 })
 
 app.delete('/api/persons/:id', (request, response) => {
@@ -82,25 +88,27 @@ app.delete('/api/persons/:id', (request, response) => {
 app.post('/api/persons', (request, response) => {
   const body = request.body
 
-  if(persons.some(person => person.name === body.name)){
-    return response.status(400).json({
-        error: 'Name already exists in the phonebook'
-      });
-  }
-  else if (!body.name && !body.number) {
-    return response.status(400).json({ 
-      error: 'content missing' 
-    })
-  }
+  // if(persons.some(person => person.name === body.name)){
+  //   return response.status(400).json({
+  //       error: 'Name already exists in the phonebook'
+  //     });
+  // }
+  // else if (!body.name && !body.number) {
+  //   return response.status(400).json({ 
+  //     error: 'content missing' 
+  //   })
+  // }
 
-  const person = {
-    id: Date.now(),
+  const person = new Person({
     name: body.name,
     number: body.number,
     
-  }
+  })
 
-  persons = persons.concat(person)
+  // persons = persons.concat(person)
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 
   response.json(person)
 })
