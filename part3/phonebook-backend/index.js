8d3477fce7c6a6c3ac52a6c83dced80c25cb68e5
parbap0 <parbap0@gmail.com>
2024-06-19 22:15:49 +0530

    minor port change

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -113,7 +113,7 @@ app.post('/api/persons', (request, response) => {
   response.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
